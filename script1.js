a1918bc86df4b65a2b9e7afbcc0ac23859f78c88
itsmeFrank <87568490+itsmeFrank@users.noreply.github.com>
2021-08-07 11:35:03 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,17 @@
+var request = new XMLHttpRequest();
+//2.opening a connection(specify the URL)
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+//3.sending a connection 
+request.send();
+//4.once the server responded successfully then we have to process the data
+request.onload=function(){
+    var data= JSON.parse(this.response);
+    console.log(data);
+    for (let i = 0 ; i < 250 ; i++) {
+    
+        // name, region, sub region and population
+
+        console.log("Country Name:",data[i].name+" "+"Region:",data[i].region+" "+"Sub Region:",data[i].subregion +" "+"Population:",data[i].population);
+        
+    }
+}
